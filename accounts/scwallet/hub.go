bf1b1767be00b0258aacb25d0af154b98231cd21
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 09:37:17 -0800

    Update hub.go

--- accounts/scwallet/hub.go
+++ accounts/scwallet/hub.go
@@ -41,10 +41,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/event"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/pictor01/ALBA/accounts"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBAevent"
+	"github.com/pictor01/ALBA/log"
 	pcsc "github.com/gballet/go-libpcsclite"
 )
 
