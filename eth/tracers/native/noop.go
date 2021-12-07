cddd13653850d2a769e0a3f3ba6beaf310b688ef
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:53:31 -0800

    Update noop.go

--- eth/tracers/native/noop.go
+++ eth/tracers/native/noop.go
@@ -21,9 +21,9 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/eth/tracers"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/vm"
+	"github.com/pictor01/ALBA/alba/tracers"
 )
 
 func init() {
