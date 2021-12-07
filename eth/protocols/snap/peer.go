af1c349a8a86362b1ae0657252bafe82a66ee589
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:33:06 -0800

    Update peer.go

--- eth/protocols/snap/peer.go
+++ eth/protocols/snap/peer.go
@@ -17,9 +17,9 @@
 package snap
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/log"
+	"github.com/pictor01/ALBA/p2p"
 )
 
 // Peer is a collection of relevant information we have about a `snap` peer.
