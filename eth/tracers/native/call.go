0e08cddcc7c5b4da5de7a07d30bdb0e5b9320baf
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:54:18 -0800

    Update call.go

--- eth/tracers/native/call.go
+++ eth/tracers/native/call.go
@@ -25,9 +25,9 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/eth/tracers"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/vm"
+	"github.com/pictor01/ALBA/alba/tracers"
 )
 
 func init() {
