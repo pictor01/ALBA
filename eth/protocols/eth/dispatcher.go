4c5d4bd39d0eddda1aa5060be157167015fac57f
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:05:25 -0800

    Update dispatcher.go

--- eth/protocols/eth/dispatcher.go
+++ eth/protocols/eth/dispatcher.go
@@ -14,14 +14,14 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package eth
+package alba
 
 import (
 	"errors"
 	"fmt"
 	"time"
 
-	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/pictor01/ALBA/p2p"
 )
 
 var (
