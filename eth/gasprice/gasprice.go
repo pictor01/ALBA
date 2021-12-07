9358255f56cf171bb5980644b9a3fb75d475add8
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 14:59:32 -0800

    Update gasprice.go

--- eth/gasprice/gasprice.go
+++ eth/gasprice/gasprice.go
@@ -22,13 +22,13 @@ import (
 	"sort"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/event"
+	"github.com/pictor01/ALBA/log"
+	"github.com/pictor01/ALBA/params"
+	"github.com/pictor01/ALBA/rpc"
 	lru "github.com/hashicorp/golang-lru"
 )
 
