9f8ae0ecaa263461cf4aefcce4404b9e38673b3b
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 14:24:50 -0800

    Update block_fetcher.go

--- eth/fetcher/block_fetcher.go
+++ eth/fetcher/block_fetcher.go
@@ -22,14 +22,14 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/prque"
-	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/eth/protocols/eth"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
-	"github.com/ethereum/go-ethereum/trie"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/common/prque"
+	"github.com/pictor01/ALBA/consensus"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/alba/protocols/alba"
+	"github.com/pictor01/ALBA/log"
+	"github.com/pictor01/ALBA/metrics"
+	"github.com/pictor01/ALBA/trie"
 )
 
 const (
