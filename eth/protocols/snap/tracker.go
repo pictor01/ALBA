87e540617fb346310b1e948bb11c11d46bda9324
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:44:12 -0800

    Update tracker.go

--- eth/protocols/snap/tracker.go
+++ eth/protocols/snap/tracker.go
@@ -19,7 +19,7 @@ package snap
 import (
 	"time"
 
-	"github.com/ethereum/go-ethereum/p2p/tracker"
+	"github.com/pictor01/ALBA/p2p/tracker"
 )
 
 // requestTracker is a singleton tracker for request times.
