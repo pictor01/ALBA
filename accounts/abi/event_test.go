385f722e8616066d94602beaca309773168265ae
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 09:20:13 -0800

    Update event_test.go

--- accounts/abi/event_test.go
+++ accounts/abi/event_test.go
@@ -25,8 +25,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/crypto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
