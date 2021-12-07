1d4f5b09f84cf08ff83ab50b1290e3f9f8571035
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:28:18 -0800

    Update tracker.go

--- eth/protocols/eth/tracker.go
+++ eth/protocols/eth/tracker.go
@@ -14,12 +14,12 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package eth
+package alba
 
 import (
 	"time"
 
-	"github.com/ethereum/go-ethereum/p2p/tracker"
+	"github.com/pictor01/ALBA/p2p/tracker"
 )
 
 // requestTracker is a singleton tracker for eth/66 and newer request times.
