46c5433ccba59b32a95ca88a045e4371ea6ac4c6
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 09:22:54 -0800

    Update pack_test.go

--- accounts/abi/pack_test.go
+++ accounts/abi/pack_test.go
@@ -27,7 +27,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/pictor01/ALBA/common"
 )
 
 // TestPack tests the general pack/unpack tests in packing_test.go
