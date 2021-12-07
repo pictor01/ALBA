424ed13a8614d1d067181c5d160f25f8da6a17e7
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:14:07 -0800

    Update handshake.go

--- eth/protocols/eth/handshake.go
+++ eth/protocols/eth/handshake.go
@@ -14,16 +14,16 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package eth
+package alba
 
 import (
 	"fmt"
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/forkid"
-	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/forkid"
+	"github.com/pictor01/ALBA/p2p"
 )
 
 const (
