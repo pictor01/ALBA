1a48e089500205b7035f156f7d157d4f25fae13d
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:55:58 -0800

    Update access_list_tracer.go

--- eth/tracers/logger/access_list_tracer.go
+++ eth/tracers/logger/access_list_tracer.go
@@ -20,9 +20,9 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/core/vm"
 )
 
 // accessList is an accumulator for the set of accounts and storage slots an EVM
