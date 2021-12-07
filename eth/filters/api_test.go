61c5f76b0a80d9f8c28b47dc674edcb809e3a138
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 14:37:12 -0800

    Update api_test.go

--- eth/filters/api_test.go
+++ eth/filters/api_test.go
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/rpc"
 )
 
 func TestUnmarshalJSONNewFilterArgs(t *testing.T) {
