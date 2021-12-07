4fc9e566921669b6fd048d9a2fc7c50299206e85
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 09:08:21 -0800

    Update util.go

--- accounts/abi/bind/util.go
+++ accounts/abi/bind/util.go
@@ -21,9 +21,9 @@ import (
 	"errors"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/log"
 )
 
 // WaitMined waits for tx to be mined on the blockchain.
