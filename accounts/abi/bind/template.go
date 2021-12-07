a911a11f93da69b306dc39ef1d13c8df9b7b21dc
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 09:06:44 -0800

    Update template.go

--- accounts/abi/bind/template.go
+++ accounts/abi/bind/template.go
@@ -16,7 +16,7 @@
 
 package bind
 
-import "github.com/ethereum/go-ethereum/accounts/abi"
+import "github.com/pictor01/ALBA/accounts/abi"
 
 // tmplData is the data structure required to fill the binding template.
 type tmplData struct {
@@ -91,13 +91,14 @@ import (
 	"math/big"
 	"strings"
 	"errors"
-
-	ethereum "github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/event"
+	"alba"
+	
+	"github.com/pictor01/ALBA"
+	"github.com/pictor01/ALBA/accounts/abi"
+	"github.com/pictor01/ALBA/accounts/abi/bind"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/event"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
