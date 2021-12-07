ad3d1651fbf4ccb25d0230f5861644f4330f7a21
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:52:26 -0800

    Update tracer.go

--- eth/tracers/native/tracer.go
+++ eth/tracers/native/tracer.go
@@ -37,7 +37,7 @@ package native
 import (
 	"errors"
 
-	"github.com/ethereum/go-ethereum/eth/tracers"
+	"github.com/pictor01/ALBA/alba/tracers"
 )
 
 // init registers itself this packages as a lookup for tracers.
