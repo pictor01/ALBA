35d6bc008c0e1c21878404f0067eaa24cfe21b52
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 14:31:23 -0800

    Update tx_fetcher_test.go

--- eth/fetcher/tx_fetcher_test.go
+++ eth/fetcher/tx_fetcher_test.go
@@ -23,10 +23,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/mclock"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/common/mclock"
+	"github.com/pictor01/ALBA/core"
+	"github.com/pictor01/ALBA/core/types"
 )
 
 var (
