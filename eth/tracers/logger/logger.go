66f8117bec7bd1732a540c4ed2618b151b1763b0
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:58:27 -0800

    Update logger.go

--- eth/tracers/logger/logger.go
+++ eth/tracers/logger/logger.go
@@ -24,12 +24,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/common/hexutil"
+	"github.com/pictor01/ALBA/common/math"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/core/vm"
+	"github.com/pictor01/ALBA/params"
 	"github.com/holiman/uint256"
 )
 
