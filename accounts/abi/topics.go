a55b4f055ae1ae384784f9c4107441c65ba2cecb
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 09:25:23 -0800

    Update topics.go

--- accounts/abi/topics.go
+++ accounts/abi/topics.go
@@ -23,8 +23,8 @@ import (
 	"math/big"
 	"reflect"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/crypto"
 )
 
 // MakeTopics converts a filter query argument list into a filter topic set.
