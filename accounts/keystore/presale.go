3ce7acda0b70edb2c9bd214dafda87648028132d
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 09:57:50 -0800

    Update presale.go

--- accounts/keystore/presale.go
+++ accounts/keystore/presale.go
@@ -25,8 +25,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pictor01/ALBA/accounts"
+	"github.com/pictor01/ALBA/crypto"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/pbkdf2"
 )
