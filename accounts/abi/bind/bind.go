3c770ebf6fad45a44018cbab19b51aa62a0fc569
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 08:12:39 -0800

    Update bind.go

--- accounts/abi/bind/bind.go
+++ accounts/abi/bind/bind.go
@@ -30,8 +30,8 @@ import (
 	"text/template"
 	"unicode"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/pictor01/ALBA/accounts/abi"
+	"github.com/pictor01/ALBA/log"
 )
 
 // Lang is a target programming language selector to generate bindings for.
