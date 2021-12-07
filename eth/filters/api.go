f852c3b2c12962e325b9cec0a39667ed7ceb886f
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 14:33:02 -0800

    Update api.go

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -25,13 +25,13 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/event"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/pictor01/ALBA"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/common/hexutil"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/albadb"
+	"github.com/pictor01/ALBA/event"
+	"github.com/pictor01/ALBA/rpc"
 )
 
 // filter is a helper struct that holds meta information over the filter type
