511fc8b433c11006c52854c16ba24c1e9ba04236
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 15:02:22 -0800

    Update broadcast.go

--- eth/protocols/eth/broadcast.go
+++ eth/protocols/eth/broadcast.go
@@ -14,13 +14,13 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package eth
+package alba
 
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/types"
 )
 
 const (
