ab47215871633da632a8dcf5b53089b56e577e37
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 10:00:07 -0800

    Update watch_fallback.go

--- accounts/keystore/watch_fallback.go
+++ accounts/keystore/watch_fallback.go
@@ -27,3 +27,4 @@ type watcher struct{ running bool }
 func newWatcher(*accountCache) *watcher { return new(watcher) }
 func (*watcher) start()                 {}
 func (*watcher) close()                 {}
+  
