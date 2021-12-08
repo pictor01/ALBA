50217a3de33ec2542e0088367f3f6cd332579252
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 16:10:09 -0800

    Update calltrace_test.go

--- eth/tracers/internal/tracetest/calltrace_test.go
+++ eth/tracers/internal/tracetest/calltrace_test.go
@@ -26,22 +26,22 @@ import (
 	"testing"
 	"unicode"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/eth/tracers"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rlp"
-	"github.com/ethereum/go-ethereum/tests"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/common/hexutil"
+	"github.com/pictor01/ALBA/common/math"
+	"github.com/pictor01/ALBA/core"
+	"github.com/pictor01/ALBA/core/rawdb"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/core/vm"
+	"github.com/pictor01/ALBA/crypto"
+	"github.com/pictor01/ALBA/alba/tracers"
+	"github.com/pictor01/ALBA/params"
+	"github.com/pictor01/ALBA/rlp"
+	"github.com/pictor01/ALBA/tests"
 
 	// Force-load native and js pacakges, to trigger registration
-	_ "github.com/ethereum/go-ethereum/eth/tracers/js"
-	_ "github.com/ethereum/go-ethereum/eth/tracers/native"
+	_ "github.com/pictor01/ALBA/alba/tracers/js"
+	_ "github.com/pictor01/ALBA/alba/tracers/native"
 )
 
 // To generate a new callTracer test, copy paste the makeTest method below into
@@ -52,8 +52,8 @@ import (
 // call trace run, assembling all the gathered information into a test case.
 var makeTest = function(tx, rewind) {
   // Generate the genesis block from the block, transaction and prestate data
-  var block   = eth.getBlock(eth.getTransaction(tx).blockHash);
-  var genesis = eth.getBlock(block.parentHash);
+  var block   = alba.getBlock(eth.getTransaction(tx).blockHash);
+  var genesis = alba.getBlock(block.parentHash);
 
   delete genesis.gasUsed;
   delete genesis.logsBloom;
