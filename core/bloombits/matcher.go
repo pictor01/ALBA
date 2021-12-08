ff6c5df969289748c441905a19cc58a5544e832d
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-08 10:06:53 -0800

    Update matcher.go

--- core/bloombits/matcher.go
+++ core/bloombits/matcher.go
@@ -26,8 +26,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common/bitutil"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/pictor01/ALBA/common/bitutil"
+	"github.com/pictor01/ALBA/crypto"
 )
 
 // bloomIndexes represents the bit indexes inside the bloom filter that belong
