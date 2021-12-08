6ba6a87febc6e27a42391db72b0ee22bd42bfe5a
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 20:22:14 -0800

    Update memorydb_test.go

--- ethdb/memorydb/memorydb_test.go
+++ ethdb/memorydb/memorydb_test.go
@@ -19,13 +19,13 @@ package memorydb
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/ethdb/dbtest"
+	"github.com/pictor01/ALBA/albadb"
+	"github.com/pictor01/ALBA/albadb/dbtest"
 )
 
 func TestMemoryDB(t *testing.T) {
 	t.Run("DatabaseSuite", func(t *testing.T) {
-		dbtest.TestDatabaseSuite(t, func() ethdb.KeyValueStore {
+		dbtest.TestDatabaseSuite(t, func() albadb.KeyValueStore {
 			return New()
 		})
 	})
