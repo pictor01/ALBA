ab5f82b731697a178a96d1f17a57d425714efbc1
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 20:18:07 -0800

    Update leveldb_test.go

--- ethdb/leveldb/leveldb_test.go
+++ ethdb/leveldb/leveldb_test.go
@@ -19,15 +19,15 @@ package leveldb
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/ethdb/dbtest"
+	"github.com/pictor01/ALBA/albadb"
+	"github.com/pictor01/ALBA/albadb/dbtest"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/storage"
 )
 
 func TestLevelDB(t *testing.T) {
 	t.Run("DatabaseSuite", func(t *testing.T) {
-		dbtest.TestDatabaseSuite(t, func() ethdb.KeyValueStore {
+		dbtest.TestDatabaseSuite(t, func() albadb.KeyValueStore {
 			db, err := leveldb.Open(storage.NewMemStorage(), nil)
 			if err != nil {
 				t.Fatal(err)
