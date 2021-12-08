9cda9f89938158e4d640c4e37798d95d815b6f46
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-08 10:47:34 -0800

    Update accessors_indexes_test.go

--- core/rawdb/accessors_indexes_test.go
+++ core/rawdb/accessors_indexes_test.go
@@ -22,11 +22,11 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/albadb"
+	"github.com/pictor01/ALBA/params"
+	"github.com/pictor01/ALBA/rlp"
 	"golang.org/x/crypto/sha3"
 )
 
