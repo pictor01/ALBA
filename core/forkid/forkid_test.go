2e05c38ddb79ba28e797409ac87c75e8329983c8
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-08 10:08:13 -0800

    Update forkid_test.go

--- core/forkid/forkid_test.go
+++ core/forkid/forkid_test.go
@@ -21,9 +21,9 @@ import (
 	"math"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/params"
+	"github.com/pictor01/ALBA/rlp"
 )
 
 // TestCreation tests that different genesis and fork rule combinations result in
