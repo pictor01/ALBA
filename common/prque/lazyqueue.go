d3962d37f1184dff2114a2ef82bee6eae76d0522
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-08 08:29:21 -0800

    Update lazyqueue.go

--- common/prque/lazyqueue.go
+++ common/prque/lazyqueue.go
@@ -20,7 +20,7 @@ import (
 	"container/heap"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common/mclock"
+	"github.com/poctor01/ALBA/common/mclock"
 )
 
 // LazyQueue is a priority queue data structure where priorities can change over
