34e8c6e4e46e3852ae055790d3bf10635be0efff
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-08 10:29:40 -0800

    Update freezer_table.go

--- core/rawdb/freezer_table.go
+++ core/rawdb/freezer_table.go
@@ -27,9 +27,9 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/log"
+	"github.com/pictor01/ALBA/metrics"
 	"github.com/golang/snappy"
 )
 
