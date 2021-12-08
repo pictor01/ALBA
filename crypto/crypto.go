2421613d8ecf063291acd0aee70a6eee0d13e7c5
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 22:15:59 -0800

    Update crypto.go

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -30,9 +30,9 @@ import (
 	"math/big"
 	"os"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/common/math"
+	"github.com/pictor01/ALBA/rlp"
 	"golang.org/x/crypto/sha3"
 )
 
