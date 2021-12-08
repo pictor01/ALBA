a7fe6c110382d423eaa8f26ebc33d7608953f2a0
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-08 10:28:04 -0800

    Update schema.go

--- core/rawdb/schema.go
+++ core/rawdb/schema.go
@@ -21,8 +21,8 @@ import (
 	"bytes"
 	"encoding/binary"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/metrics"
 )
 
 // The fields below define the low level database schema prefixing.
