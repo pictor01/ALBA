84d04362da56a8d5e290699dced0fac0735a04fd
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 16:31:40 -0800

    Update sync_test.go

--- eth/sync_test.go
+++ eth/sync_test.go
@@ -21,11 +21,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/eth/downloader"
-	"github.com/ethereum/go-ethereum/eth/protocols/eth"
-	"github.com/ethereum/go-ethereum/eth/protocols/snap"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/pictor01/ALBA/alba/downloader"
+	"github.com/pictor01/ALBA/alba/protocols/alba"
+	"github.com/pictor01/ALBA/alba/protocols/snap"
+	"github.com/pictor01/ALBA/p2p"
+	"github.com/pictor01/ALBA/p2p/enode"
 )
 
 // Tests that snap sync is disabled after a successful sync cycle.
