2a8e4f96d287b95a6f9067940e9384acd505fc65
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 16:17:46 -0800

    Update queue.go

--- eth/downloader/queue.go
+++ eth/downloader/queue.go
@@ -26,11 +26,11 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/prque"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/common/prque"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/log"
+	"github.com/pictor01/ALBA/metrics"
 )
 
 const (
