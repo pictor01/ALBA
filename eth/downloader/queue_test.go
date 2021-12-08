af949e0b2b45a5271974fb68736af5c5a023bc57
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-07 16:17:09 -0800

    Update queue_test.go

--- eth/downloader/queue_test.go
+++ eth/downloader/queue_test.go
@@ -24,14 +24,14 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus/ethash"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/trie"
+	"github.com/pictor01/ALBA/common"
+	"github.com/pictor01/ALBA/consensus/ethash"
+	"github.com/pictor01/ALBA/core"
+	"github.com/pictor01/ALBA/core/rawdb"
+	"github.com/pictor01/ALBA/core/types"
+	"github.com/pictor01/ALBA/log"
+	"github.com/pictor01/ALBA/params"
+	"github.com/pictor01/ALBA/trie"
 )
 
 var (
