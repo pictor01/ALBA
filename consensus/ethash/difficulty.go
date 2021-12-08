1f4cc1e42468bfdac5c2c04fc0bb7ce1d81d17ee
pictor01 <95560577+pictor01@users.noreply.github.com>
2021-12-08 08:54:04 -0800

    Update difficulty.go

--- consensus/ethash/difficulty.go
+++ consensus/ethash/difficulty.go
@@ -14,12 +14,12 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package ethash
+package albaash
 
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pictor01/ALBA/core/types"
 	"github.com/holiman/uint256"
 )
 
